3cdf1cb42099483ad58fe88ab91a08a690631edd
황준혁 <strawji02@naver.com>
2023-07-18 12:14:59 +0900

    feat: :sparkles: add apps page scroll
    앱 서랍 스크롤 가능하게 추가

--- src/components/Apps/Apps.tsx
+++ src/components/Apps/Apps.tsx
@@ -4,30 +4,78 @@ import FindMy from '../../assets/Find My.svg';
 import Phone from '../../assets/Phone.svg';
 import Photos from '../../assets/Photos.svg';
 import Safari from '../../assets/Safari.svg';
+import { useElementSize } from '../../hooks/useElementSize';
 import AppButton, { AppButtonProps } from './AppButton';
 
-const AppsGrid = styled.div`
+const AppsGrid = styled.div<{ $width?: number }>`
   grid-template-columns: repeat(auto-fill, minmax(25%, auto));
+  grid-template-rows: repeat(auto-fill, minmax(120px, auto));
   display: grid;
   margin: 20px 0;
+  width: ${(props) => (props.$width ? `${props.$width}px` : '100%')};
+  scroll-snap-align: start;
+`;
+
+const AppSlide = styled.div`
+  height: 100%;
+  position: relative;
+  overflow-y: scroll;
+  scroll-snap-type: x mandatory;
+  list-style: none;
+  padding: 0;
+  z-index: 1;
+  display: block;
+  overscroll-behavior: contain;
+`;
+
+const AppWrapper = styled.div`
+  height: 100%;
+  display: inline-flex;
+  position: absolute;
+  left: 0;
 `;
 
 function Apps() {
-  const icons: AppButtonProps[] = [
-    { label: 'strawji', src: Contacts },
-    { label: 'Skills', src: Photos },
-    { label: 'Pojects', src: Safari },
-    { label: 'About me', src: FindMy },
-    { label: 'Contact', src: Phone },
+  const apps: AppButtonProps[][] = [
+    [
+      { label: 'strawji', src: Contacts },
+      { label: 'Skills', src: Photos },
+      { label: 'Pojects', src: Safari },
+      { label: 'About me', src: FindMy },
+      { label: 'Contact', src: Phone },
+    ],
+    [
+      { label: 'strawji', src: Contacts },
+      { label: 'Skills', src: Photos },
+      { label: 'Pojects', src: Safari },
+      { label: 'About me', src: FindMy },
+      { label: 'Contact', src: Phone },
+    ],
+    [
+      { label: 'strawji', src: Contacts },
+      { label: 'Skills', src: Photos },
+      { label: 'Pojects', src: Safari },
+      { label: 'About me', src: FindMy },
+      { label: 'Contact', src: Phone },
+    ],
   ];
+  const { ref, width } = useElementSize<HTMLDivElement>();
+
   return (
-    <div>
-      <AppsGrid>
-        {icons.map((props) => (
-          <AppButton {...props} />
+    <AppSlide ref={ref}>
+      <AppWrapper>
+        {apps.map((icons, appsIdx) => (
+          <AppsGrid $width={width} key={`app-grid-${appsIdx}-page`}>
+            {icons.map((props, iconsIdx) => (
+              <AppButton
+                key={`icon-${appsIdx}-app-${iconsIdx}-icon`}
+                {...props}
+              />
+            ))}
+          </AppsGrid>
         ))}
-      </AppsGrid>
-    </div>
+      </AppWrapper>
+    </AppSlide>
   );
 }
 
